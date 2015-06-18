8e7a1515d1e59bb4250e25f40e405f70c82c9b5c
cfa532 <cfa532@hotmail.com>
2015-06-18 16:22:13 +0800

    new html5

---
+++ h5/mobile/js/tabs.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+$(function(){	
+	$('.sy-tab ul li').click(function(){
+		$(this).addClass('nav-active').siblings().removeClass('nav-active');
+		$('.list-info>div:eq('+$(this).index()+')').show().siblings().hide();	
+	})
+	
+	//上传图片高度等于宽度
+	function autoHeight(){
+		var height=$(".send-img li").width();
+		$(".send-img li").css("height",height);
+		}
+	window.onload=autoHeight;
+	window.onresize=autoHeight;
+	
+	//删除图片
+	$(".send-img li i").click(function(){
+		$(this).parent().hide();
+		})
+	
+})
+
+//文件选择
+ function fileSelect() {
+        document.getElementById("fileToUpload").click(); 
+    }	
---
+++ h5/readme.txt
@@ -0,0 +1,3 @@
+因为我们h5用的这个架构，所以你做前端的时候，使用这些css，js,在这个基础上做。
+
+http://mobileangularui.com/
